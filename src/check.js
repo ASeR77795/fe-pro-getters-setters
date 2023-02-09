334c421b31c0379a1b193aba1851ccc0b6724470
Pavel Fedorenko <winterspray@gmail.com>
2023-02-09 21:32:05 +0200

    final

--- src/check.js
+++ src/check.js
@@ -3,3 +3,15 @@ console.log(school.aGradeStudents); // 'Ivan'
 console.log(school.bGradeStudents); // 'Eugene'
 console.log(school.cGradeStudents); // 'Maria, Dmytro, Anna'
 console.log(school.dGradeStudents); //'Anton, Olha, Bohdan'
+
+//   get() {
+//     const scores = Object.values(school.students).filter((student) => {
+//       return student.averageGrade >= 90 && student.averageGrade <= 100;
+//     });
+//     const names = scores.map((item) => {
+//       return item.name;
+//     });
+//     const result = names.join(', ');
+//     return result;
+//   },
+// },
--- src/index.js
+++ src/index.js
@@ -22,46 +22,88 @@ export const school = {
     7: new Student('Ivan', [76, 89, 78, 98, 98, 99, 89, 96]),
   },
 };
-Object.defineProperty(school, 'aGradeStudents', {
-  enumerable: true,
-  get() {
-    const scores = Object.values(school.students).filter((student) => {
-      return student.averageGrade >= 90 && student.averageGrade <= 100;
-    });
-    const names = scores.map((item) => {
-      return item.name;
-    });
-    const result = names.join(', ');
-    return result;
+Object.defineProperties(school, {
+  aGradeStudents: {
+    get() {
+      return Object.values(this.students)
+        .filter(
+          (student) => student.averageGrade >= 90 && student.averageGrade <= 100
+        )
+        .map((item) => item.name)
+        .join(', ');
+    },
+  },
+  bGradeStudents: {
+    get() {
+      return Object.values(this.students)
+        .filter(
+          (student) => student.averageGrade >= 75 && student.averageGrade <= 89
+        )
+        .map((item) => item.name)
+        .join(', ');
+    },
+  },
+  cGradeStudents: {
+    get() {
+      return Object.values(this.students)
+        .filter(
+          (student) => student.averageGrade <= 75 && student.averageGrade >= 60
+        )
+        .map((item) => item.name)
+        .join(', ');
+    },
+  },
+  dGradeStudents: {
+    get() {
+      return Object.values(this.students)
+        .filter(
+          (student) => student.averageGrade <= 59 && student.averageGrade >= 0
+        )
+        .map((item) => item.name)
+        .join(', ');
+    },
   },
 });
-Object.defineProperty(school, 'bGradeStudents', {
-  get() {
-    return Object.values(this.students)
-      .filter(
-        (student) => student.averageGrade >= 75 && student.averageGrade <= 89
-      )
-      .map((item) => item.name)
-      .join(', ');
-  },
-});
-Object.defineProperty(school, 'cGradeStudents', {
-  get() {
-    return Object.values(this.students)
-      .filter(
-        (student) => student.averageGrade <= 75 && student.averageGrade >= 60
-      )
-      .map((item) => item.name)
-      .join(', ');
-  },
-});
-Object.defineProperty(school, 'dGradeStudents', {
-  get() {
-    return Object.values(this.students)
-      .filter(
-        (student) => student.averageGrade <= 59 && student.averageGrade >= 0
-      )
-      .map((item) => item.name)
-      .join(', ');
-  },
-});
+// Object.defineProperty(school, 'aGradeStudents', {
+//   enumerable: true,
+//   get() {
+//     const scores = Object.values(school.students).filter((student) => {
+//       return student.averageGrade >= 90 && student.averageGrade <= 100;
+//     });
+//     const names = scores.map((item) => {
+//       return item.name;
+//     });
+//     const result = names.join(', ');
+//     return result;
+//   },
+// });
+// Object.defineProperty(school, 'bGradeStudents', {
+//   get() {
+//     return Object.values(this.students)
+//       .filter(
+//         (student) => student.averageGrade >= 75 && student.averageGrade <= 89
+//       )
+//       .map((item) => item.name)
+//       .join(', ');
+//   },
+// });
+// Object.defineProperty(school, 'cGradeStudents', {
+//   get() {
+//     return Object.values(this.students)
+//       .filter(
+//         (student) => student.averageGrade <= 75 && student.averageGrade >= 60
+//       )
+//       .map((item) => item.name)
+//       .join(', ');
+//   },
+// });
+// Object.defineProperty(school, 'dGradeStudents', {
+//   get() {
+//     return Object.values(this.students)
+//       .filter(
+//         (student) => student.averageGrade <= 59 && student.averageGrade >= 0
+//       )
+//       .map((item) => item.name)
+//       .join(', ');
+//   },
+// });
